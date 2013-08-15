5bad9dd7b2d38a8655ef1d6d86c9c199063d9df3
jmaldonadoscitechperu.com <jmaldonado@scitechperu.com>
2013-08-15 02:30:58 -0500

    Inicio - Lazo presentación final

--- contenido/inicio.php
+++ contenido/inicio.php
@@ -6,10 +6,12 @@
                 <div class="news_detail">
                     <span>
                         + <a href="/noticias">Emisión de bonos de titulación de Odebrendt Inversiones Inmobiliarias S.A.C.</a><br>
+                        <div class="texto_blanco">El Estudio Lazo, De Romaña & Gagliuffi asesoró...</div>
                         + <a href="/noticias">Venta del 100% de acciones de las principales empresas del Grupo Refasa - Carrión.</a><br>
+                        <div class="texto_blanco">El Estudio Lazo, De Romaña & Gagliuffi asesoró...</div>
                         + <a href="/noticias">Emisión de bonos de titulación de Los Portales S.A.</a><br>
-                        <br>
-                        <a href="/noticias" title="" class="buttonLink right"><img src="images/layout/ver-mas.png" /></a>
+                        <div class="texto_blanco">El Estudio Lazo, De Romaña & Gagliuffi asesoró...</div>
+                        <a href="/noticias" title="" class="buttonLink right"><img src="images/layout/ver-mas-mini.png" /></a>
                     </span>
                 </div>
             </div>
@@ -19,7 +21,7 @@
                 <h2>RESULTADOS EFICIENTES Y EFICACES<br>
                     <span class="lowercase">Somos un estudio con visión</span>
                 </h2>
-                <p>Lazo, de Ronaña & Gagluffi Lazo, de Romaña & Gagluffi cierra operación con BBVA Lazo, de Romañan & Gagluffi Lazo, de Ronaña & Gagluffi Lazo, de Romaña & Gagluffi cierra operación con BBVA Lazo, de Romañan & Gagluffi .</p>
+                <p>Somos un equipo con una visión moderna del ejercicio profesional conjugando lo práctico y lo teórico, con altos valores éticos y que busca que sus clientes obtengan resultados eficientes y eficaces.</p>
                 <a href="#" title="" class="buttonLink"><img src="images/layout/ver-mas.png" /></a>
             </div>
             <div class="one_half"><img src="images/layout/content-image-1.jpg" /></div>
@@ -95,10 +97,10 @@ require_once "areas.html";
         <div id="footer">
             <div class="container footer">
                 <div class="one_half"></div>
+                <div class="texto_naranja">El estudio ha sido reconocido por:</p>
                 <div class="two_thirds">
-                    <div class="libros"><a href="/reconocimientos"><img src="images/layout/libro-1.jpg" /></a></div>
                     <div class="libros"><a href="/reconocimientos"><img src="images/layout/libro-2.jpg" /></a></div>
-                    <div class="libros"><a href="/reconocimientos"><img src="images/layout/libro-3.jpg" /></a></div>
+                    <div class="libros"><a href="/reconocimientos"><img src="images/layout/libro-1.jpg" /></a></div>
                 </div>
             </div>
         </div>
